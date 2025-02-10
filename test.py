6e554a0863a9b12fbaa363511cdfe7f3fb4da2c0
Chirag Patel <143702330+chira603@users.noreply.github.com>
2025-02-10 16:39:34 +0530

    Update test.py for raising error for semicoloum

--- test.py
+++ test.py
@@ -4,17 +4,18 @@ from parser import parse, ParseError
 
 # Successful test cases
 tests_ok = [
-    "x = 10; y = 20;z =5; x + y*z",              # Should evaluate to 30
-    "a = 5; b = (a = a + 3) + a; a + b",    # a becomes 8, then b becomes (8 + 8) = 16, so result = 8 + 16 = 24
-    "5 + 3*7",                              # Single expression: 8
-    "if 1 == 1 { 10 } else { 20 }",        # Conditional: 10
+    "x = 10; y = 20;z =5; x + y*z;",              # Should evaluate to 30
+    "a = 5; b = (a = a + 3) + a; a + b;",    # a becomes 8, then b becomes (8 + 8) = 16, so result = 8 + 16 = 24
+    "5 + 3*7;",                              # Single expression: 8
+    "if 1 == 1 { 10 } else { 20 };",        # Conditional: 10
 ]
 
 # Failing test cases
 tests_error = [
     "if 1 == 1 { 5",                      # missing closing brace
     "if 2 < 3 { 4 } else if { 5 }",         # missing condition after 'else if'
-    "y + 2",                              # undefined variable 'y'
+    "y + 2;",                              # undefined variable 'y'
+    "x =3 y=4"
 ]
 
 def run_tests():
